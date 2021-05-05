3896105a476fe742f8a5e0976def96792313eba0
samizdat <alfred.delay@gmail.com>
2021-05-05 15:36:00 +0200

    Fixing the add function

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,7 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + +b }, // ajout d'un problème.
+    'add':      function(a,b) { return +a + +b }, // Bug fixed.
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
